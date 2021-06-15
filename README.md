8f45a0d513ee44dcac882c5ef3e96ddbdb54a803
iShyness <spestionv@gmail.com>
2021-06-15 21:30:25 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# Aramboot
-Boot Skin in Aram Mode LeagueOfLeagends
+# ✨ lol-aram-boost
+
+Skin boosts a League of Legends ARAM lobby without consuming any RP, temporarily unlocking a skin for everyone and granting you extra BE at the end of your match.
+
+# ⚡ Usage
+
+1. Open your client (be logged in)
+2. Run the script after logging in (your client is visible) or when you're in champion select (Note: Run as administrator if your client is also running as that, otherwise it will give you an error)
+3. Follow the instructions
+4. Your lobby should be skin boosted!
+
+# 🔧 Requirements
+
+### Downloading compiled
+
+You can download lol-aram-boost [right here](https://github.com/x00bence/lol-aram-boost/releases/latest). You can simply run the `.exe` file and not bother with compiling.
+
+### Building from source
+
+You must have Python 3+ installed on your PC, along with the `requests` module. 
+
+- [Python 3](https://www.python.org/downloads/)
+- [requests](https://pypi.org/project/requests/)
+
+After installing Python, you can install requests from the command prompt with either of these commands: `pip install requests` or `python -m pip install requests`.
+
+# ⏪ History
+
+- September 13, 2020: lol-aram-boost is released.
+- February 23, 2021: Riot Games cracks down on user made scripts, in turn patching lol-aram-boost. A new method is found not much later.
+- April 26, 2021: lol-aram-boost is updated to use the new method.
---
+++ aramboot.py
@@ -0,0 +1,42 @@
+import subprocess
+import re
+try:
+    import requests
+    import urllib3
+except ModuleNotFoundError:
+    print('[!] You are missing the "requests" module.\nYou may install it via either of the following commands: "pip install requests" or "python -m pip install requests".\n\nPress any key to exit.')
+    input()
+    exit()
+
+if __name__ == '__main__':
+    try:        
+        print('$ lol-aram-boost | x00bence\n')
+
+        # Batch command to get the client process
+        command = "WMIC PROCESS WHERE name='LeagueClientUx.exe' GET commandline"
+
+        # Execute the command
+        output = subprocess.Popen(command, stdout=subprocess.PIPE,
+                                shell=True).stdout.read().decode('utf-8')
+
+        # Extract needed args
+        port = re.findall(r'"--app-port=(.*?)"', output)[0]
+        password = re.findall(r'"--remoting-auth-token=(.*?)"', output)[0]
+
+        # Disable the annoying certificate error
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+        # Set up session
+        session = requests.session()
+        session.verify = False
+
+        print('[*] Connected to League; Press Enter in champion select to boost lobby, ^C to exit.')
+
+        # Running in an infinite loop, so the user doesn't have to restart the script all the time
+        while True:
+            input()
+            session.post('https://127.0.0.1:%s/lol-login/v1/session/invoke?destination=lcdsServiceProxy&method=call&args=["","teambuilder-draft","activateBattleBoostV1",""]' %
+                    port, data={}, auth=requests.auth.HTTPBasicAuth('riot', password))
+            print('[+] Boosted the lobby!')
+    except KeyboardInterrupt:
+        exit()
